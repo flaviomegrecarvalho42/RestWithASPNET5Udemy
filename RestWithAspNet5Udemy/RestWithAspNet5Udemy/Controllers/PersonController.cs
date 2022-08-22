fe25b1227eda053675b3ff634782bb5e428912ca
Flavio Megre Carvalho <flavio.megre@gmail.com>
2022-08-22 14:43:42 -0300

    Working with four verbs

---
+++ RestWithAspNet5Udemy/RestWithAspNet5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+
+namespace RestWithAspNet5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+                return BadRequest("Invalid Input");
+
+            var sumNumbers = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+            return Ok(sumNumbers.ToString());
+        }
+
+        private static bool IsNumeric(string strNumber)
+        {
+            return double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out _);
+        }
+
+        private static decimal ConvertToDecimal(string strNumber)
+        {
+            if (!decimal.TryParse(strNumber.Replace(",", "."), out decimal decimalValue))
+                return 0;
+
+            return decimalValue;
+        }
+    }
+}
